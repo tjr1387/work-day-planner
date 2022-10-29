536fb8d6929902ca64d0d3908554a1dd343efcf1
Hair_Trigg3r <tylerjreimer@gmail.com>
2022-10-29 15:49:57 -0400

    Add main table & style tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,55 @@
 // Element selectors
 
+
 // Global Variables
+
+
+
+// Array of all hours (a row for each) for the creation loop
+const hoursArray = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+
+// Grabs the current time and formats the current hour to match the syntax of hoursArray
+const currentHour = moment().format('hA');
+
 
 // Functions/Logic
 
-    // will need something that grabs the current day (using 'moment') and puts it in the header
+// This takes in the hour and returns one of three classes (past, present, future) for the bg-color of the textareas
+    // May want to use isBefore and isAfter
+function assignAreaColor(hour) {
+    if (hour === currentHour) {
+        return 'present'
+    }
+}
+
+
+
+// Sets the current date to today and formats it
+$("#currentDay").text(moment().format('MMMM Do, YYYY'));
+
+// Create the the main content with a loop for each row
+    // Lot will go into this because I'll want to assign id/classes here
+    // I can probly as I build the table do the coresponding bg-color for textareas here -- maybe a helper function?
+
+for (hour of hoursArray) {
+    // Creates the row and its columns, with some bootstrap classes, as well the hour label (from array)
+    const rowEl = $('<section>').addClass('row');
+        // The last two classes of the hour element I added for positioning
+    const hourEl = $('<div>').addClass('hour col-1 text-right pt-3').text(hour);
+    const textAreaEl = $('<textarea>').addClass('col-10');
+    // Calls the helper function to assign a bg-color to the textarea
+    textAreaEl.addClass(`${assignAreaColor(hourEl[0].innerText)}`);
+        // The last three classes I added here are to put the icon 'span' in the center of the save element
+    const saveEl = $('<div>').addClass('col-1 saveBtn d-flex align-items-center justify-content-center');
+
+    // Creates/adds the disk icon to the save element;
+    saveEl.append($('<span>').html('&#128190'));
+
+    // Adds the three columns to the row
+    rowEl.append(hourEl, textAreaEl, saveEl);
+    // Adds the row to the container
+    $('.container').append(rowEl);
+}
 
     // now for hte main content, a BUNCH of stuff needed here
         // using Moment, will be color coding blocks for past hours, the current hour, and the future hours
@@ -14,8 +59,6 @@
         //the main trick here is the logic behind how to grab each of those three specifically
     
     // the next bit is allowing users to enter data into the 'textarea' column of each row, and saving it
-        // *a click event on the 'textarea' of each row (probly by a class specific to the column)
-        // allow it to stay there even when the focus is turned to a differnet column (but not saved)
         // *another event listener for when the 'save' button is hit, but ONLY for that row
             // heres where youll be doing localstorage, is on this button hit -- but only for the row
             // youll have to save JUST the text in that row in local storage, so itll be the content of the textarea AND the hour it matches
@@ -26,4 +69,7 @@
         //this should again be pretty similar to what you did last week
 
     // i think the toughest part will be deinifing ids/classes on the html elements in order to single them out
-    // 
+
+
+
+
