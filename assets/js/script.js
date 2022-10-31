432bd867e4a53ea58fbd74cc94ac152e81f8a145
Hair_Trigg3r <tylerjreimer@gmail.com>
2022-10-31 14:46:16 -0400

    Add localStorage append/update; style tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,13 +37,14 @@ for (hour of hoursArray) {
         // The last two classes of the hour element I added for positioning
     const hourEl = $('<p>').addClass('hour col-1 text-right pt-3').text(hour);
     const textAreaEl = $('<textarea>').addClass('col-10');
-    // Calls the helper function to assign a bg-color to the textarea
-    textAreaEl.addClass(`${assignAreaColor(hourEl[0].innerText)}`);
         // The last three classes I added here are to put the icon 'span' in the center of the save element
     const saveEl = $('<div>').addClass('col-1 saveBtn d-flex align-items-center justify-content-center');
 
+    // Calls the helper function to assign a bg-color to the textarea
+    textAreaEl.addClass(`${assignAreaColor(hourEl[0].innerText)}`);
+
     // Creates/adds the disk icon to the save element;
-    saveEl.append($('<span>').html('&#128190'));
+    saveEl.append($('<span>').html('&#128190').addClass('saveIcon'));
 
     // Adds the three columns to the row
     rowEl.append(hourEl, textAreaEl, saveEl);
@@ -51,14 +52,45 @@ for (hour of hoursArray) {
     $('.container').append(rowEl);
 }
 
+// Load up (persist) the elements with text from localStorage
+    // Once this is done, I am done!
 
-    
-    // the next bit is allowing users to enter data into the 'textarea' column of each row, and saving it
-        // * event listener for when the 'save' button is hit, but ONLY for that row
-            // heres where youll be doing localstorage, is on this button hit -- but only for the row
-            // youll have to save JUST the text in that row in local storage, so itll be the content of the textarea AND the hour it matches
-            // deifnitely do the array of objects thing with local storage like you did last week
-                // {eventText; ''whatever appts/tasks', hour: [the hour(row) it corresponds to]}
-    
-    // once localstorage is set up properly, youll also need to set up the 'get' when the page is reloaded -- and it will have to go AFTER youve built the table
-        //this should again be pretty similar to what you did last week
+
+// Event listener for the 'save this textarea's value' click event
+    // It's on the parent (row), but only activates when the save button (or it's child icon) is clicked
+    // It then jumps back into the parent (row) to grab the hour and textarea's value; then saves them to localStorage
+$('.row').click(function (event) {
+    const saveText = event.target;
+    if (saveText.matches('.saveBtn') || saveText.matches('.saveIcon')) {
+        // Grabs the hour and the plan text
+        const hour = event.currentTarget.children[0].innerText;
+        const planText = event.currentTarget.children[1].value;
+        // Everything that follows will only run if there's actually text in the textarea
+        if (planText) {
+            // Brings up local storage
+            const storedPlans = JSON.parse(localStorage.getItem("plans"));
+            // Builds an object to be put into storage (though it won't get used in the 'replace' case ahead..)
+            const entry = {time: hour, text: planText}
+            if (!storedPlans) {
+                // If there's nothing in local storage, store the object we just built in an array
+                localStorage.setItem("plans", JSON.stringify([entry]));
+            } else {
+                let isReplaced = false;     // Set a flag to be toggled if theres an entry for the hour
+                // If there is storage, look through its array to see if there's already an entry (object) for this particular hour
+                for (let planObj of storedPlans) {
+                    // If this hour already has an entry, we'll overwrite it; just replace the text value with the current one; don't need the object we built
+                    if (planObj.time === hour) {
+                        planObj.text = planText;
+                        isReplaced = true;      // Change the flag to reflect that an entry was found and replaced
+                    }
+                }
+                // If the current time wasn't found (therefore no update/replace was made), then just push the object to the storage array
+                if (!isReplaced) {
+                    storedPlans.push(entry);
+                }
+                // Put our changed array (whether we pushed to it OR changed an existing entry) back into localStorage
+                localStorage.setItem("plans", JSON.stringify(storedPlans));
+            }
+        }
+    }
+});
